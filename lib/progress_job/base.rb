bd74c8967ac93803a306faff8aaec450998f83e0
Hunter Hillegas <hunter@lastonepicked.com>
2017-05-28 10:06:32 -0700

    stubbing out for testing

--- lib/progress_job/base.rb
+++ lib/progress_job/base.rb
@@ -6,16 +6,19 @@ module ProgressJob
 
     def before(job)
       @job = job
-      job.update_column(:progress_max, @progress_max)
-      job.update_column(:progress_current, 0)
+
+      unless job.new_record?
+        job.update_column(:progress_max, @progress_max)
+        job.update_column(:progress_current, 0)
+      end
     end
 
     def update_progress(step: 1)
-      @job.update_column(:progress_current, @job.progress_current + step)
+      @job.update_column(:progress_current, @job.progress_current + step) unless @job.new_record?
     end
 
     def update_stage(stage)
-      @job.update_column(:progress_stage, stage)
+      @job.update_column(:progress_stage, stage) unless @job.new_record?
     end
 
     def update_stage_progress(stage, step: 1)
@@ -24,7 +27,7 @@ module ProgressJob
     end
 
     def update_progress_max(progress_max)
-      @job.update_column(:progress_max, progress_max)
+      @job.update_column(:progress_max, progress_max) unless @job.new_record?
     end
 
     def error(job, exception)
